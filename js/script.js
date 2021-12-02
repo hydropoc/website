dca5f65b8600a66f5327e98be1de1daa21bde731
Paul <bl0cksb3rg@gmail.com>
2021-12-02 08:59:58 +0100

    test

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     console.log("loaded");
 
-    $.post('http://194.62.1.75:3000/api/data/add', JSON.stringify({
+    $.post('http://192.168.20.28:3000/api/data/add', JSON.stringify({
         "sensor_pt1000": 1234,
         "sensor_eco2": 1234,
         "sensor_tvoc": 1234,
