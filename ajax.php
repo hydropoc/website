2a544c366c63e3fcecdf517103d9b2fb6fa50204
CHRISTIAN\Sonic <aso@aso.wtf>
2021-12-09 21:23:58 +0100

    jo

--- ajax.php
+++ ajax.php
@@ -1,6 +1,7 @@
 <?php
     date_default_timezone_set("Europe/Berlin");
     $labels = $_POST["labels"];
+    $actuator = json_decode($_POST["actuator"], true);
     if (is_array($labels) && count($labels) >= 13) {
         $lastLabel = $labels[array_key_last($labels)];
         $date = date_create(date_format(date_create(), "d.m.Y") . " " . $lastLabel);
@@ -17,6 +18,52 @@
             array_push($labels, date_format($date, "H:i:s"));
         }
     }
+    // Wenn Simulation bereits läuft
+    if ($actuator["count"] >= 1) {
+        //
+    }
+    else {
+        // Wennn Simulation nicht läuft, zufallsbedingt starten
+        if (random_int(0, 5) == 1) {
+            // Zufallsbedingte Dauer des Aktors festlegen (Spanne von N Label, steigende Aktorleistung parallel mit steigendem aktuellen Wert)
+            $actuatorDuration = random_int(4, 7);
+            $currentValues = [];
+            if (random_int(0, 1) == 1) {
+                // Zu hohen Wert normalisieren (80+)
+                $wtf = 80;
+                for ($i = 1; $i <= $actuatorDuration; $i++) {
+                    // wtf darf nie vor Schleifenende auf Sollwert sinken!
+                    $wtf -= 0;
+                    if (count($currentValues) >= 1) {
+                        //
+                    }
+                    else {
+                        //
+                    }
+                }
+            }
+            else {
+                // Zu niedrigen Wert normalisieren (20-)
+                $wtf = 20;
+                for ($i = 1; $i <= $actuatorDuration; $i++) {
+                    // wtf darf nie vor Schleifenende auf Sollwert steigen!
+                    $wtf -= 0;
+                    if (count($currentValues) >= 1) {
+                       // array_push($currentValues, )
+                    }
+                    else {
+                        //
+                    }
+                }
+            }
+            //array_push($currentValues, (count($currentValues) >= 1 ? random_int($currentValues[array_key_last($currentValues)] + $i, $currentValues[array_key_last($currentValues)] + random_int(1,5)) : random_int(1 ,10)));
+        }
+        else {
+            // Zufallsdaten ohne Aktoren erzeugen
+            $current = ["x" => $labels[array_key_last($labels)], "y" => rand(5,95)];
+            $middle = ["x" => $labels[array_key_last($labels)], "y" => rand(50,53)];
+        }
+    }
     $data = [
         "co2" => [
             "labels" => [
@@ -25,8 +72,8 @@
             ],
             "data" => [
                 "current" => ["x" => $labels[array_key_last($labels)], "y" => rand(5,95)], // Aktueller Wert
-                "setpoint" => ["x" => $labels[array_key_last($labels)], "y" => rand(50,60)], // Sollwert
-                "middle" => ["x" => $labels[array_key_last($labels)], "y" => rand(50,55)], // Mittelwert
+                "setpoint" => ["x" => $labels[array_key_last($labels)], "y" => 52], // Sollwert
+                "middle" => ["x" => $labels[array_key_last($labels)], "y" => rand(50,53)], // Mittelwert
                 "upperControlLimit" => ["x" => $labels[array_key_last($labels)], "y" => 90],
                 "upperWarningLimit" => ["x" => $labels[array_key_last($labels)], "y" => 80],
                 "lowerControlLimit" => ["x" => $labels[array_key_last($labels)], "y" => 10],
