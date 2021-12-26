4695a7de010950f84bb81c92bd4765b895743fde
CHRISTIAN\Sonic <aso@aso.wtf>
2021-12-26 01:59:43 +0100

    Ach du meine Sterne!

--- dataTables_test.php
+++ dataTables_test.php
@@ -25,6 +25,15 @@
         case "plants":
             $data[] = ["Riesenrafflesie", "Buddhas Hand", "Verpiss-dich-Pflanze"];
             break;
+        case "useraccounts":
+            $data[] = ["displayname" => "Deine Mutter", "cn" => "deimuddah", "created" => "11.05.2000", "status" => "offline", "options" => "<button>lol</button>"];
+            break;
+        case "pumps":
+            for ($i = 1; $i <= 6; $i++) {
+                $isOn = rand(0, 5);
+                $data[] = ["name" => "Pumpe " . $i, "rpm" => $isOn == 2 ? rand(1000, 2000) : 0, "power" => $isOn == 2 ? rand(70, 80) : 0, "status" => "<input type='checkbox' data-toggle='toggle' data-size='sm' data-on='Ein' data-off='Aus' " . ($isOn == 1 ? "checked" : null) . ">"];
+            }
+            break;
         default:
             $data = null;
             break;
