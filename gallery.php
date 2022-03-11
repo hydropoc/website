1c139657c7d624b8a4808c31f05c48a4678fca43
Christian Sander <christian.sander@awi.de>
2022-03-11 14:47:21 +0100

    Mutter, der Mann mit dem Koks ist da.

--- gallery.php
+++ gallery.php
@@ -1,4 +1,4 @@
 <?php
-    echo json_encode(["images" => ["test1.jpg", "test2.jpg", "test3.jpg", "test4.jpg", "test5.jpg"]]);
+    echo json_encode(["images" => ["test1.jpg", "test2.jpg", "test3.jpg", "test4.jpg", "test5.jpg", "test6.jpg", "test7.jpg"]]);
     return;
 ?>
